4d5ede394b5a34e9c67034a0fb6cd9e4f651285b
rgrishin <roman1988grishin@gmail.com>
2020-07-27 08:37:11 +0300

    navbar brand logo

--- Uspeak/Uspeak/ClientApp/src/components/NavMenu.js
+++ Uspeak/Uspeak/ClientApp/src/components/NavMenu.js
@@ -25,10 +25,10 @@ export class NavMenu extends Component {
     return (
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <a href="/">
-              <img src="uspeak-logo.png" alt="uspeak" />
-            </a>
+        <Container>
+            <NavbarBrand href="/">
+                <img src="uspeak-logo.png" alt="uspeak" />
+            </NavbarBrand>
             <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
