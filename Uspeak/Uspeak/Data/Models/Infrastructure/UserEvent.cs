889b981b8df502ea8431477f06136a74b45f733b
rgrishin <roman1988grishin@gmail.com>
2020-08-02 21:35:28 +0300

    user events

---
+++ Uspeak/Uspeak/Data/Models/Infrastructure/UserEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace Uspeak.Data.Models.Infrastructure
+{
+    public enum UserEvent
+    {
+        [Description("зашел на сайт")]
+        System_LoggedIn = 0,
+        [Description("вышел из сайта")]
+        System_LoggedOut = 1,
+        [Description("попытался зайти на сайт")]
+        System_LogInTry = 2,
+
+        [Description("создал пользователя")]
+        Account_CreatedUser = 100,
+        [Description("сменил пароль")]
+        Account_ChangedPassword = 101,
+        [Description("сменил настройки пользователя")]
+        Account_Changed= 102,
+        [Description("удалил пользователя")]
+        Account_Deleted = 103,
+
+        [Description("открыл")]
+        Entity_HasOpened = 200,
+        [Description("закрыл")]
+        Entity_HasClosed = 201,
+        [Description("создал")]
+        Entity_Created = 202,
+        [Description("изменил")]
+        Entity_Changed = 203,
+        [Description("удалил")]
+        Entity_Deleted = 203,
+
+    }
+}
