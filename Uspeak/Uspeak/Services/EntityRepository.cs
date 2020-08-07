2dd4cf7edcfdcc59666430b0ee429f4848a7d102
rgrishin <roman1988grishin@gmail.com>
2020-08-07 13:37:51 +0300

    entity repository

---
+++ Uspeak/Uspeak/Services/EntityRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Uspeak.Data.Models;
+using Uspeak.Persistence;
+using Uspeak.Services.Exceptions;
+
+namespace Uspeak.Services
+{
+    public class EntityRepository
+    {
+        private readonly IContextFactory _contextFactory;
+        public EntityRepository(IContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public Task<List<Entity>> GetEntities(EntityStatus status)
+        {
+            using (var context = _contextFactory.Create())
+            {
+                return context.Entities.Where(x => x.Status == status).ToListAsync();
+            }
+        }
+
+        public Task<List<Entity>> GetEntities(EntityStatus status, EntityType type)
+        {
+            using (var context = _contextFactory.Create())
+            {
+                return context.Entities.Where(x =>
+                    x.Status == status
+                    && x.EnityKind == type).ToListAsync();
+            }
+        }
+
+        public Task<List<Entity>> GetEntities(EntityType type)
+        {
+            using (var context = _contextFactory.Create())
+            {
+                return context.Entities.Where(x => x.EnityKind == type).ToListAsync();
+            }
+        }
+
+        public Task<List<Entity>> GetEnities(EntityStatus status, DateTime statusSetEarlierThen,
+            DateTime? statusSetLaterThen)
+        {
+            if (statusSetLaterThen != null && statusSetLaterThen > statusSetEarlierThen )
+                throw new WrongTimePeriodsException($"{nameof(statusSetLaterThen)} ({statusSetLaterThen}) больше, чем " +
+                                                    $"{nameof(statusSetEarlierThen)} ({statusSetEarlierThen}) -> периоды времени не пересекаются.");
+            
+            using (var context = _contextFactory.Create())
+            {
+                IQueryable<Entity> query = context.Entities.Where(x => x.Status == status
+                                            && x.StatusChangedTime <= statusSetEarlierThen);
+                if (statusSetLaterThen != null)
+                    query = query.Where(x => x.StatusChangedTime >= statusSetLaterThen);
+                return query.ToListAsync();
+            }
+        }
+    }
+}
---
+++ Uspeak/Uspeak/Services/Exceptions/WrongTimePeriodsException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uspeak.Services.Exceptions
+{
+    /// <summary>
+    /// Указаны непересекающиеся дипазано временных промежутков, условие "И" никогда не будет выполнено.ы
+    /// </summary>
+    public class WrongTimePeriodsException : ArgumentException
+    {
+        private readonly string _message;
+
+        public WrongTimePeriodsException(string message)
+        {
+            _message = message;
+        }
+
+        public sealed override string Message => _message;
+    }
+}
