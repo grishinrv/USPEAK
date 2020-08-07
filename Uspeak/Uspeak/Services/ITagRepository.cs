ff4362f030848db61b416b94424e40ac13fee013
rgrishin <roman1988grishin@gmail.com>
2020-08-07 21:32:10 +0300

    + tag repository

---
+++ Uspeak/Uspeak/Services/ITagRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Uspeak.Data.Models;
+
+namespace Uspeak.Services
+{
+    public interface ITagRepository
+    {
+        /// <summary>
+        /// Получить список тэгов по типу тэга, и по статусу сущностей.
+        /// </summary>
+        /// <param name="tagTypetype">тип тэга</param>
+        /// <param name="entityType">статус сущности</param>
+        /// <returns>Список действующих тэгов по типу.</returns>
+        Task<List<Tag>> GetTags(TagType tagType, EntityType entityType);
+    }
+}
---
+++ Uspeak/Uspeak/Services/TagRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Uspeak.Data.Models;
+using Uspeak.Persistence;
+
+namespace Uspeak.Services
+{
+    public class TagRepository : ITagRepository
+    {
+        private readonly IContextFactory _contextFactory;
+
+        public TagRepository(IContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+        
+        public Task<List<Tag>> GetTags(TagType tagType, EntityType entityType)
+        {
+            using (var context = _contextFactory.Create())
+            {
+                return context.Entities
+                    .Where(x=> x.EnityKind == entityType)
+                    .SelectMany<Entity, Tag>(t => t.EntityTags.Select(e => e.Tag).Where(a =>a.TagKind == tagType))
+                    .Distinct()
+                    .ToListAsync();
+            }
+        }
+    }
+}
