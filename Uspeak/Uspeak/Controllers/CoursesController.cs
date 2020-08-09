8e0b0e8883379a7317df8ba27b4be731b878c06a
rgrishin <roman1988grishin@gmail.com>
2020-08-09 17:51:10 +0300

    course controller

--- Uspeak/Uspeak/Controllers/CoursesController.cs
+++ Uspeak/Uspeak/Controllers/CoursesController.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Uspeak.Data.Models;
+using Uspeak.Data.Models.Courses;
+using Uspeak.Services;
 
 namespace Uspeak.Controllers
 {
@@ -10,6 +13,14 @@ namespace Uspeak.Controllers
     [Route("[controller]")]
     public class CoursesController : ControllerBase
     {
+        private readonly ICourseRepository _courseRepository;
 
+        public CoursesController(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        [HttpGet]
+        public Task<List<Course>> GetCourses(Guid subjectId) => _courseRepository.GetCoursesByTagId(subjectId);
     }
 }
