ab77055faa37e72834d2b06e35759d1b54d0c6d8
rgrishin <roman1988grishin@gmail.com>
2020-08-16 18:44:59 +0300

    nav menu clean

--- Uspeak/Uspeak/ClientApp/src/components/NavMenu.js
+++ Uspeak/Uspeak/ClientApp/src/components/NavMenu.js
@@ -33,13 +33,7 @@ export class NavMenu extends Component {
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Главная страница</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/counter">Счетчик</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/QuestionTemplate">Создать вопрос</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/">Предметы</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
