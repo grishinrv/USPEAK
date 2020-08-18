b1c889f2d9c806fa7f42dcd1748a3186ed0dd169
rgrishin <roman1988grishin@gmail.com>
2020-08-18 04:53:19 +0300

    Home controller

---
+++ Uspeak/Uspeak/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Uspeak.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("Home")]
+        [Route("Home/Index")]
+        [Route("Home/Index/{id?}")]
+        [HttpGet]
+        public IActionResult Index(int? id)
+        {
+            return View();
+        }
+    }
+}
