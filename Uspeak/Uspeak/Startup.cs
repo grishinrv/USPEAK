9b73f8a1b748b2a2a54d47c4f81a389e2fa37546
rgrishin <roman1988grishin@gmail.com>
2020-08-18 05:32:43 +0300

    startup

--- Uspeak/Uspeak/Startup.cs
+++ Uspeak/Uspeak/Startup.cs
@@ -6,6 +6,10 @@ using Microsoft.Extensions.Hosting;
 using NLog;
 using Uspeak.Persistence;
 using Uspeak.Services;
+using Microsoft.AspNetCore.Http;
+using JavaScriptEngineSwitcher.V8;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using React.AspNet;
 
 namespace Uspeak
 {
@@ -21,6 +25,13 @@ namespace Uspeak
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+
+            // Make sure a JS engine is registered, or you will get an error!
+            services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName)
+              .AddV8();
+
             services.AddControllersWithViews(mvcOtions =>
             {
                 mvcOtions.EnableEndpointRouting = false;
@@ -43,6 +54,26 @@ namespace Uspeak
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Initialise ReactJS.NET. Must be before static files.
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                //config
+                //  .AddScript("~/js/First.jsx")
+                //  .AddScript("~/js/Second.jsx");
+
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //  .SetLoadBabel(false)
+                //  .AddScriptWithoutTransform("~/js/bundle.server.js");
+            });
 
             app.UseStaticFiles();
 
