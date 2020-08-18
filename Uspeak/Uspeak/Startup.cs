8ad4590890fb38d631e3e4ce3d8940f5d82b2659
rgrishin <roman1988grishin@gmail.com>
2020-08-18 04:21:09 +0300

    Startup - switch from endpoints to MVC

--- Uspeak/Uspeak/Startup.cs
+++ Uspeak/Uspeak/Startup.cs
@@ -1,16 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Net.Http.Headers;
 using NLog;
-using System;
-using System.IO;
-using Uspeak.Infrastructure;
 using Uspeak.Persistence;
 using Uspeak.Services;
 
@@ -28,11 +21,9 @@ namespace Uspeak
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
-            // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
+            services.AddControllersWithViews(mvcOtions =>
             {
-                configuration.RootPath = "ClientApp/build";
+                mvcOtions.EnableEndpointRouting = false;
             });
 
             services.AddSingleton<Infrastructure.IConfigurationProvider>(x =>
@@ -52,34 +43,14 @@ namespace Uspeak
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseSpaStaticFiles();
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapControllerRoute(
+                routes.MapRoute(
                     name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
-            });
-
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseReactDevelopmentServer(npmScript: "start");
-                }
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
