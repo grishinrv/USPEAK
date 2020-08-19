9b9139abf3ca319af202f8d7dd271e38cfab22a7
rgrishin <roman1988grishin@gmail.com>
2020-08-19 10:39:31 +0300

    progress

---
+++ Uspeak/Uspeak/webpack.config.js
@@ -0,0 +1,40 @@
+﻿var path = require(path);
+var config = {
+    entry = {
+        home: "./ClientApp/Home.js", // HomeModule - ??
+    },
+    output: {
+        path: path.resolve(__dirname, "wwwroot/publish"),
+        filename: "[name].bundle.js",
+        publicPath: 'publish/'
+    },
+    mode: process.env.NODE_ENV === | 'production' ? 'production' : 'development',
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    optimization: {
+        splitChunks: {
+            cachedGroups: {
+                commons: {
+                    name: "commons",
+                    chunks: "initial",
+                    minChunks: 2,
+                    minSize: 0
+                }
+            }
+        },
+        occurrenceOrder: true
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader',
+            }
+        ]
+    }
+};
+
+module.exports = config;
