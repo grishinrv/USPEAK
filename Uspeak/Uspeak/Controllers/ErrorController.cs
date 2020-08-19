18b6af364ef799443da0584b9a72ee998beeb363
rgrishin <roman1988grishin@gmail.com>
2020-08-20 00:51:26 +0300

    error controller (log routing and other infrastructure errors)

---
+++ Uspeak/Uspeak/Controllers/ErrorController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+
+namespace Uspeak.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class ErrorController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public ErrorController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Index()
+        {
+            // Retrieve error information in case of internal errors
+            var error = HttpContext
+                      .Features
+                      .Get<IExceptionHandlerFeature>();
+            if (error != null)
+            {
+                // Use the information about the exception 
+                var exception = error.Error;
+                _logger.Trace($"{exception.Message} {exception.StackTrace}");
+            }
+        }
+    }
+}
