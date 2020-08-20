c2f2174ec2e8316183b1c86d7c5562b75c0bd0f4
rgrishin <roman1988grishin@gmail.com>
2020-08-20 06:44:39 +0300

    tech user select permissions

--- Uspeak/Uspeak/Sql/Permissions.sql
+++ Uspeak/Uspeak/Sql/Permissions.sql
@@ -7,3 +7,17 @@ USE [Uspeak]
 GO
 CREATE USER [UspeakTech] FOR LOGIN [UspeakTech] WITH DEFAULT_SCHEMA=[dbo]
 GO
+
+
+Use [Uspeak];
+GRANT SELECT ON "dbo"."Tags" TO [UspeakTech];
+GRANT SELECT ON "dbo"."User" TO [UspeakTech];
+GRANT SELECT ON "dbo"."TestSettings" TO [UspeakTech];
+GRANT SELECT ON "dbo"."QuestionSettings" TO [UspeakTech];
+GRANT SELECT ON "dbo"."Images" TO [UspeakTech];
+GRANT SELECT ON "dbo"."EntitiesTags" TO [UspeakTech];
+GRANT SELECT ON "dbo"."Entities" TO [UspeakTech];
+GRANT SELECT ON "dbo"."Courses" TO [UspeakTech];
+GRANT SELECT ON "dbo"."Entities" TO [UspeakTech];
+GRANT SELECT ON "dbo"."Config" TO [UspeakTech];
+GRANT SELECT ON "dbo"."AnswerOption" TO [UspeakTech];
