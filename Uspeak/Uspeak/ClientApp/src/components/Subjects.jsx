9543047122ff8c704d954c43fda4a18440f06644
rgrishin <roman1988grishin@gmail.com>
2020-08-22 16:01:47 +0300

    jss preprocessing for flag

--- Uspeak/Uspeak/ClientApp/src/components/Subjects.jsx
+++ Uspeak/Uspeak/ClientApp/src/components/Subjects.jsx
@@ -1,5 +1,34 @@
 import React, { Component } from 'react';
 import flexStyle from '../styles/flex.module.css';
+import {createUseStyles} from 'react-jss'
+
+const flagStyle = createUseStyles({
+  eng: {
+    background: {
+      color: '#00247d',
+      size: '100%'
+    },
+    position: 'relative',
+    '&:before': {
+      position: 'absolute',
+      content: "",
+      top: 0,
+      left: 0,
+      background: 'linear-gradient(180deg,transparent 40%, #cc142b 0,#cc142b 60%,transparent 0),\n' +
+        '  linear-gradient(90deg,transparent 45%,#cc142b 0,#cc142b 55%,transparent 0),\n' +
+        '  linear-gradient(180deg,transparent 35%,#fff 0,#fff 65%,transparent 0),\n' +
+        '  linear-gradient(90deg,transparent 40%,#fff 0,#fff 60%,transparent 0),\n' +
+        '  linear-gradient(146deg,transparent 50%,#cc142b 0,#cc142b 53%,transparent 0) -65% 45%,\n' +
+        '  linear-gradient(146deg,transparent 50%,#cc142b 0,#cc142b 53%,transparent 0) 55% -45%,\n' +
+        '  linear-gradient(34deg,transparent 50%,#cc142b 0,#cc142b 53%,transparent 0) -75% -43%,\n' +
+        '  linear-gradient(34deg,transparent 50%,#cc142b 0,#cc142b 53%,transparent 0) 70% 46%,\n' +
+        '  linear-gradient(146deg,transparent 45%,#fff 0,#fff 55%,transparent 0),\n' +
+        '  linear-gradient(34deg,transparent 45%,#fff 0,#fff 55%,transparent 0)',
+      width: '100%',
+      height: '100%',
+      'background-repeat': 'no-repeat'
+    }
+  }})
 
 export class Subjects extends Component {
   static displayName = Subjects.name;
@@ -13,9 +42,13 @@ export class Subjects extends Component {
     this.getSubjects();
   }
 
+  static mapFlag(cssClass) {
+    return flagStyle.eng;
+  }
+
   static renderSubjects(subjects) {
     const items = subjects.map(subject =>
-      <div key={'key_'+subject.id} className={[flexStyle.flexItem, subject.cssClass].join(" ")}  />
+      <div key={'key_'+subject.id} className={[flexStyle.flexItem, flagStyle.eng].join(" ")}  />
     );
     return (
       <div className={flexStyle.flexList}>
