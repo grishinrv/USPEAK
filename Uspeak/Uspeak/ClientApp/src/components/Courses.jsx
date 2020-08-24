7ec76c7fb940941297ca09754f6b5ec4c1040fb9
rgrishin <roman1988grishin@gmail.com>
2020-08-24 15:37:57 +0300

    courses component

---
+++ Uspeak/Uspeak/ClientApp/src/components/Courses.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import flexStyle from "../styles/flex.module.css";
+
+export default function  Courses (props) {
+  const [courses, setCourses] =  useState([]);
+  const [loading, setLoading] =  useState(true);
+
+  async function getCourses(subjectId) {
+    var url = new URL('Courses'),
+        params = {'subjectId':subjectId.toString()};
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+    const response = await fetch(url);
+    const data = await response.json();
+    setCourses(data);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    getCourses(props.subjectId);
+  }, []);
+
+  let contents = this.state.loading
+    ? <p><em>Loading...</em></p>
+    : Courses.renderSubjects(this.state.subjects);
+  return (
+    <div>
+      <h1 style={{"textAlign": "center"}}>this.state.header</h1>
+      {contents}
+    </div>
+  );
+}
