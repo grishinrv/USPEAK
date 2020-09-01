5746d716426c04cbc6da715bada8a96d32b1fed6
rgrishin <roman1988grishin@gmail.com>
2020-09-01 19:54:42 +0300

    ef migration: asbout us config, ru courses, evo subject

--- Uspeak/Uspeak/Migrations/20200901164435_asbout us config, ru courses, evo subject.Designer.cs
+++ Uspeak/Uspeak/Migrations/20200901164435_asbout us config, ru courses, evo subject.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Uspeak.Persistence;
 
 namespace Uspeak.Migrations
 {
     [DbContext(typeof(UspeakDbContext))]
-    partial class UspeakDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200901164435_asbout us config, ru courses, evo subject")]
+    partial class asboutusconfigrucoursesevosubject
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
